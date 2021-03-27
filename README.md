add3c38013de0c9d203c599fc0efd147ae58b217
Shreyas Kapur <shreyask@mit.edu>
2021-03-27 06:00:05 -0400

    Init scaffolding.

---
+++ README.md
@@ -0,0 +1,22 @@
+# VaxFlow
+
+How does the vaccine get to you? How equitable is access across the country? Attempt to create a visualization of the COVID19 vaccine supply chain pieced together using multiple sources of data.
+
+## Development
+
+First time setup,
+
+```
+cd vis
+npm install
+```
+
+Then, for local development,
+
+```
+npm run serve
+```
+
+## Deployment
+
+Auto gh-pages deployment coming soon.
---
+++ vis/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="centered-container">
+      <Main></Main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Main from "@/components/Main";
+
+export default {
+  name: "App",
+  components: { Main },
+};
+</script>
+
+<style lang="scss">
+html {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "IBM Plex Sans";
+}
+
+.centered-container {
+  max-width: 30rem;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+}
+</style>
