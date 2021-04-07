11f36db50deef7345bffd8f45ab6819b70329cf5
Shreyas Kapur <shreyask@mit.edu>
2021-04-07 09:58:38 -0400

    Fix wrangle

--- scripts/race_zipcode.py
+++ scripts/race_zipcode.py
@@ -6,9 +6,9 @@ import requests
 from pandas import read_excel
 from openpyxl import load_workbook
 
-f = open("race_zipcode.csv", "w")
+f = open("../data/race_zipcode.csv", "w")
 
-fname = 'zipcodes.xlsx'
+fname = '../data/zipcodes.xlsx'
 #my_sheet = 'Race and ethnicity - muni.' # change it to your sheet name, you can find your sheet name at the bottom left of your excel file
 wb = load_workbook(fname)
 ws = wb[wb.sheetnames[1]]
@@ -19,7 +19,7 @@ for row in ws.iter_rows(min_row=1, max_col=7):
     for cell in row:
         val = str(cell.value)
         if 'ZCTA5' in val and len(val) > 5:
-            row_str = val.split(" ")[1]
+            row_str = f'"{val.split(" ")[1]}"'
             reset_row_str = False
             break
         if val == "Estimate":
@@ -34,7 +34,7 @@ for row in ws.iter_rows(min_row=1, max_col=7):
             val = 'Asian'
         if 'Pacific' in val:
             val = 'NH/PI'
-        row_str += ',' + val 
+        row_str += ',' + f'"{val}"'
 
     if reset_row_str:
         row_str = row_str.strip(',')
