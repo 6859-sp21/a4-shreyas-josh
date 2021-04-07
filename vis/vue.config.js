c9bb51dee421dc073d697fcb8abde087fd0b0b26
Shreyas Kapur <shreyask@mit.edu>
2021-04-07 11:06:27 -0400

    Deploy?

--- vis/vue.config.js
+++ vis/vue.config.js
@@ -1,14 +1,5 @@
-// module.exports = {
-//     chainWebpack: (config) => {
-//       const svgRule = config.module.rule('svg');
-   
-//       svgRule.uses.clear();
-   
-//       svgRule
-//         .use('babel-loader')
-//         .loader('babel-loader')
-//         .end()
-//         .use('vue-svg-loader')
-//         .loader('vue-svg-loader');
-//     },
-//   };
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/a4-shreyas-josh/'
+        : '/'
+}
